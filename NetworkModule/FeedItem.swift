8d5982f2af486723a00589aa8ce083b8f0b74b87
Poonam <Poonamyadav464@gmail.com>
2024-12-31 15:28:12 +0530

    adding files

---
+++ NetworkModule/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  NetworkModule
+//
+//  Created by poonam on 31/12/24.
+//
+
+import Foundation
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL?
+}
